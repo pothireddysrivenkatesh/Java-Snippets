858844549b12718185e629a288da647a74dadab8
pothireddysrivenkatesh <39511380+pothireddysrivenkatesh@users.noreply.github.com>
2018-05-22 15:18:59 +0530

    Add files via upload

---
+++ Java Basics/Hello2.java
@@ -0,0 +1,9 @@
+/* Passing parameters to main method 
+   and printing them to console output.*/
+
+public class Hello2{
+public static void main(String args[]){
+System.out.println(args[0]);
+System.out.println(args[1]);
+}
+}
