89ad7f09060f7b5efd26c9cf34a0d2fcbbd40848
pothireddysrivenkatesh <39511380+pothireddysrivenkatesh@users.noreply.github.com>
2018-05-22 15:04:46 +0530

    Create Hello1.java

---
+++ Java Basics/Hello1.java
@@ -0,0 +1,13 @@
+// Code to overload main method
+
+public class Hello1{
+public static void main(String args[]){
+System.out.println("Hello Everyone");
+}
+static public void main(String a){
+System.out.println("Main method with a string argument");
+}
+public static void main(int b){
+System.out.println("Main method with int argument");
+}
+}
