21be44e9a006e821595e30517a83f11059a0f12f
pothireddysrivenkatesh <39511380+pothireddysrivenkatesh@users.noreply.github.com>
2018-05-23 22:48:19 +0530

    Add files via upload

---
+++ Java Basics/Prot.java
@@ -0,0 +1,9 @@
+//protected access modifier
+
+package home1;
+public class Prot{
+protected int tables;
+protected void printt(){
+System.out.println(tables);
+}
+}
---
+++ Java Basics/Prot2.java
@@ -0,0 +1,9 @@
+//To access protected members from other package
+
+import home1.Prot;
+class Prot2 extends Prot{
+public static void main(String args[]){
+Prot p1=new Prot();
+
+}
+}
