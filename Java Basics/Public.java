5d7360d1238d43c643d478f5d001c8d546cef0af
pothireddysrivenkatesh <39511380+pothireddysrivenkatesh@users.noreply.github.com>
2018-05-23 22:14:37 +0530

    Add files via upload

---
+++ Java Basics/Public.java
@@ -0,0 +1,9 @@
+//Public access modifier
+
+package home;
+public class Public{
+public int chairs;
+public void print(){
+System.out.println(chairs);
+}
+}
---
+++ Java Basics/Public2.java
@@ -0,0 +1,12 @@
+//Accessing public instance variables 
+//and methods from other packages.
+
+
+//import home.*;
+public class Public2{
+public static void main(String args[]){
+Public p1=new Public();
+p1.chairs=100;
+p1.print();
+}
+}
