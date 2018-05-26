ea320431b345beee4602acdbc35012c43e33db5c
pothireddysrivenkatesh <39511380+pothireddysrivenkatesh@users.noreply.github.com>
2018-05-26 23:21:40 +0530

    Add files via upload

---
+++ Data Types and Objects/objects.java
@@ -0,0 +1,11 @@
+//Object creation and hash values.
+
+class objects{
+public static void main(String args[]){
+objects ob1;
+ob1=new objects();
+objects ob2 = new objects();
+System.out.println(ob1);
+System.out.println(ob2);
+}
+}
