5ad39897c627361c57714d2048a3cc266e746816
pothireddysrivenkatesh <39511380+pothireddysrivenkatesh@users.noreply.github.com>
2018-05-26 22:39:47 +0530

    Add files via upload

---
+++ Data Types and Objects/datatypes1.java
@@ -0,0 +1,22 @@
+// Data types more on int literals and char datatype.
+
+class datatypes1{
+public static void main(String srgs[]){
+int decimalLiteral = 100;
+int octalLiteral = 0100;
+int hexaLiteral = 0x9F;
+char charValue = 'a';
+char charIntValue = 97;
+char charHexaValue = 0x00ff;
+char charUniValue = '\u0093';
+int binaryLiteral = 0b11110011;
+System.out.println("Decimal Literal = "+decimalLiteral);
+System.out.println("Octal Literal = "+octalLiteral);
+System.out.println("Hexa Literal = "+hexaLiteral);
+System.out.println("Binary Literal = "+binaryLiteral);
+System.out.println("Char Value = "+charValue);
+System.out.println("Char Int Value = "+charIntValue);
+System.out.println("Char Hexa Value = "+charHexaValue);
+System.out.println("Char Uni Value = "+charUniValue);
+}
+}
