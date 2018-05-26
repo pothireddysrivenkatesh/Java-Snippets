bda1ecbbbc30d616d6d7b683372e92e598099742
pothireddysrivenkatesh <39511380+pothireddysrivenkatesh@users.noreply.github.com>
2018-05-26 22:39:23 +0530

    Create Datatypes.java

---
+++ Data Types and Objects/Datatypes.java
@@ -0,0 +1,22 @@
+// Data types
+class datatypes{
+public static void main(String args[]){
+
+boolean b_value=true;
+byte by_value=127;
+short sh_value = 32_766;
+int int_value = 2_12_26_345;
+long long_value = 25_25_63_63_789l;
+float float_value = 3_123.456f;
+double double_value = 56_789.789456;
+char char_value = 'a';
+System.out.println("Boolean value = "+b_value);
+System.out.println("Byte value = "+by_value);
+System.out.println("Short Value = "+sh_value);
+System.out.println("Int value = "+int_value);
+System.out.println("Long Value = "+long_value);
+System.out.println("Float Value = "+float_value);
+System.out.println("Double Value = "+double_value);
+System.out.println("Char value = "+char_value);
+}
+}
