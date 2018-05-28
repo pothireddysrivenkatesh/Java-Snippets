54c823d3a28fcf00102cf9a31b8f24b2c647caab
pothireddysrivenkatesh <39511380+pothireddysrivenkatesh@users.noreply.github.com>
2018-05-28 18:23:41 +0530

    Add files via upload

---
+++ Data Types and Objects/LogicalOperators.java
@@ -0,0 +1,12 @@
+// Logical Operators
+
+class LogicalOperators{
+public static void main(String args[]){
+int x = 5;
+int y = 9;
+System.out.println(x>10||x++>5);
+System.out.println(x);
+System.out.println(y>9 && ++y>1);
+System.out.println(y);
+}
+}
