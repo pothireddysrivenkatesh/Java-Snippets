934c9ad31675e0e956a347485bbfe114875bd503
pothireddysrivenkatesh <39511380+pothireddysrivenkatesh@users.noreply.github.com>
2018-05-28 15:58:16 +0530

    Add files via upload

---
+++ Data Types and Objects/RelationalOperators.java
@@ -0,0 +1,22 @@
+// Relational Operators
+
+class RelationalOperators{
+public static void main(String args[]){
+int i1 = 10;
+int i2 = 20;
+System.out.println(i1<=i2);
+float f1 = 10.0f;
+float f2 = 20.0f;
+System.out.println(f1>=f2);
+
+System.out.println(i1==i2);
+System.out.println(i1!=i2);
+
+boolean b1 = false;
+System.out.println(b1 == true);
+System.out.println(b1 == false);
+System.out.println(b1 != true);
+System.out.println(b1 != false);
+System.out.println(i1 == f1);
+}
+}
