facdd244cacdef2d78b8d2c5b8964474b4143cce
pothireddysrivenkatesh <39511380+pothireddysrivenkatesh@users.noreply.github.com>
2018-06-04 09:34:54 +0530

    Create foreach.java

---
+++ Flow Control/foreach.java
@@ -0,0 +1,12 @@
+//for loop.
+class foreach{
+public static void main(String args[]){
+System.out.println("The elements in string are:");
+for (int i =0;i<args.length;++i,next()){
+System.out.println(args[i]);
+}
+}
+static void next(){
+System.out.println("The next element in the string is");
+}
+}
